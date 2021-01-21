058d50e9e130ec8b89da1fc75879bb647018231f
Nick <nick@collab.ee>
2021-01-22 00:42:18 +0900

    style input elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
         </Header>
 
         <Section>
-          <div>
+          <OwnerRepoBlock>
             <div>
               <Label>Owner</Label>
-              <div>
+              <InputSelect>
                 <img
                   alt=""
                   src="https://avatars2.githubusercontent.com/u/60929317?s=40&amp;v=4"
@@ -24,21 +24,14 @@ function App() {
                   height="20"
                 />
                 <span>caffenick</span>
-              </div>
+              </InputSelect>
             </div>
+            <Divider>/</Divider>
             <div>
-              <Label>repository name</Label>
-              <div>
-                <span>scaling-pancake</span>
-                <img
-                  alt=""
-                  src="https://avatars2.githubusercontent.com/u/60929317?s=40&amp;v=4"
-                  width="20"
-                  height="20"
-                />
-              </div>
+              <Label>Repository name</Label>
+              <InputText type="text" />
             </div>
-          </div>
+          </OwnerRepoBlock>
 
           <p>
             {'Great repository names are short and memorable. Need inspiration? How about '}
@@ -49,42 +42,57 @@ function App() {
             <Label>
               Description <Span>(optional)</Span>
             </Label>
-            <input type="text" />
+            <InputText type="text" fullWidth />
           </div>
         </Section>
 
         <Section>
-          <FormSelect>
-            <FormInput type="radio" checked></FormInput>
-            <Label>Public</Label>
-            <Span>Anyone on the internet can see this repository. You choose who can commit.</Span>
-          </FormSelect>
-          <FormSelect>
-            <FormInput type="radio"></FormInput>
-            <Label>Add a README file</Label>
-            <Span>You choose who can see and commit to this repository.</Span>
-          </FormSelect>
+          <InputWrapper>
+            <InputRadio type="radio" checked />
+            <LabelGroup>
+              <Label>Public</Label>
+              <Span>
+                Anyone on the internet can see this repository. You choose who can commit.
+              </Span>
+            </LabelGroup>
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputRadio type="radio" />
+            <LabelGroup>
+              <Label>Add a README file</Label>
+              <Span>You choose who can see and commit to this repository.</Span>
+            </LabelGroup>
+          </InputWrapper>
         </Section>
 
         <Section>
           <h4>Initialize this repository with:</h4>
           <p>Skip this step if you’re importing an existing repository.</p>
 
-          <FormSelect>
-            <FormInput type="checkbox"></FormInput>
-            <Label>Add a README file</Label>
-            <Span>This is where you can write a long description for your project.</Span>
-          </FormSelect>
-          <FormSelect>
-            <FormInput type="checkbox"></FormInput>
-            <Label>Add .gitignore</Label>
-            <Span>Choose which files not to track from a list of templates.</Span>
-          </FormSelect>
-          <FormSelect>
-            <FormInput type="checkbox"></FormInput>
-            <Label>Choose a license</Label>
-            <Span>A license tells others what they can and can't do with your code.</Span>
-          </FormSelect>
+          <InputWrapper>
+            <InputCheckbox type="checkbox" />
+            <LabelGroup>
+              <Label>Add a README file</Label>
+              <Span>This is where you can write a long description for your project.</Span>
+            </LabelGroup>
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputCheckbox type="checkbox" />
+            <LabelGroup>
+              <Label>Add .gitignore</Label>
+              <Span>Choose which files not to track from a list of templates.</Span>
+            </LabelGroup>
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputCheckbox type="checkbox" />
+            <LabelGroup>
+              <Label>Choose a license</Label>
+              <Span>A license tells others what they can and can't do with your code.</Span>
+            </LabelGroup>
+          </InputWrapper>
         </Section>
 
         <Section>
@@ -100,13 +108,13 @@ export default App;
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  max-width: 768px;
+  background-color: white;
 `;
 
 const Main = styled.main`
+  max-width: 768px;
   margin: 40px auto;
   padding: 0 16px;
-  // background-color: #eeeeee;
 `;
 
 const Header = styled.header`
@@ -122,10 +130,9 @@ const Header = styled.header`
 `;
 
 const Section = styled.section`
-  margin-bottom: 20px;
-  border: 1px solid silver;
-  // border-bottom: 1px solid #eaecef;
-  // border-bottom: 1px solid white;
+  padding-bottom: 16px;
+  margin-bottom: 16px;
+  border: 1px solid #eaecef;
 
   h4 {
     margin-top: 0;
@@ -136,6 +143,21 @@ const Section = styled.section`
     margin-top: 0;
     margin-bottom: 10px;
   }
+`;
+
+const OwnerRepoBlock = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Divider = styled.div`
+  font-size: 22px;
+  margin: 24px 8px 2px 8px;
+`;
+
+const LabelGroup = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const Label = styled.label`
@@ -149,13 +171,53 @@ const Span = styled.span`
   font-weight: 400;
 `;
 
-const FormSelect = styled.div`
-  margin-top: 0px;
-  margin-bottom: 16px;
-  padding-left: 20px;
+const InputBase = css`
+  border: 1px solid #e1e4e8;
+  background-color: #fafbfc;
+  border-radius: 6px;
+  outline: none;
+  padding: 5px 12px;
+  font-size: 14px;
+  line-height: 20px;
 `;
 
-const FormInput = styled.input`
-  float: left;
-  margin: 5px 0 0 -20px;
+const InputSelect = styled.div`
+  ${InputBase}
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  img {
+    border-radius: 50%;
+  }
+  span {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20px;
+    padding-left: 4px;
+  }
+`;
+
+const InputText = styled.input`
+  ${InputBase}
+  ${({ fullWidth }) =>
+    fullWidth &&
+    css`
+      width: 100%;
+    `}
+`;
+
+const InputRadio = styled.input`
+  margin-right: 4px;
+  margin-top: 8px;
+`;
+
+const InputCheckbox = styled.input`
+  margin-right: 6px;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 0px;
+  margin-bottom: 16px;
 `;
